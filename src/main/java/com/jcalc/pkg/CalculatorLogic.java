71f9d0091a9dc530d7d254fb6df623a29ae6923c
NN-DMt <oj17@hotmail.co.uk>
2017-08-28 21:21:39 +0100

    template code for non-GUI logic added

--- src/main/java/com/jcalc/pkg/CalculatorLogic.java
+++ src/main/java/com/jcalc/pkg/CalculatorLogic.java
@@ -1,7 +1,30 @@
 package com.jcalc.pkg;
 
+import java.util.Scanner;
+
 public class CalculatorLogic {
 
-	
-	
+	public static int total = 0;
+
+	// feel free to change anything written in this program,
+	// I'm mostly writing template code so would change it myself anyway
+	public static void main(String[] args) {
+		// simple console based main for testing calculator logic
+		int firstNum = 0;
+		int secondNum = 0;
+		try (Scanner scan = new Scanner(System.in);) {
+			System.out.println("Please enter two numbers to add in the console below: ");
+			firstNum = scan.nextInt();
+			secondNum = scan.nextInt();
+		} catch (Exception ie) {
+			System.out.println("not able to get user input as int");
+			return;
+		}
+		System.out.println("the answer is: " + addTwoIntegers(firstNum, secondNum));
+	}
+
+	public static int addTwoIntegers(int firstNum, int secondNum) {
+		return firstNum + secondNum;
+	}
+
 }
