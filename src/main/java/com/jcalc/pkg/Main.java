46f3453a6f4166a6b340d15a64a518f86be6b3b9
NN-DMt <oj17@hotmail.co.uk>
2017-09-03 22:00:31 +0100

    adding javafx code for event handlerm

--- src/main/java/com/jcalc/pkg/Main.java
+++ src/main/java/com/jcalc/pkg/Main.java
@@ -1,9 +1,13 @@
 package com.jcalc.pkg;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 
 
 public class Main extends Application {
@@ -19,7 +23,29 @@ public class Main extends Application {
 			root.getChildren().add(calculator);
 			primaryStage.setTitle("JCalc - Java FX calculator");
 			primaryStage.setScene(scene);
-			primaryStage.show();
+			Button equalsButton = new Button();			
+			equalsButton.setLayoutY(200);
+			equalsButton.setLayoutY(200);
+			root.getChildren().add(equalsButton);
+			
+			  Button btn = new Button();
+			    btn.setText("'Hello World'");
+			    btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			        @Override
+			        public void handle(ActionEvent event) {
+			            System.out.println("Hello World!");
+			        }
+			    });
+			
+			    btn.setLayoutX(250);
+			    btn.setLayoutY(220);
+			    root.getChildren().add(btn);
+			  //  primaryStage.setScene(new Scene(root, 300, 250));
+			    primaryStage.show();
+			
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
