9f04a66cf3264b3315e1694f2de478b65a30a600
NN-DMt <oj17@hotmail.co.uk>
2017-09-04 21:40:15 +0100

    code that needs to be refactored added :)

---
+++ src/main/java/com/jcalc/pkg/InputEventHandlers.java
@@ -0,0 +1,5 @@
+package com.jcalc.pkg;
+
+public class InputEventHandlers {
+
+}
--- src/main/java/com/jcalc/pkg/Main.java
+++ src/main/java/com/jcalc/pkg/Main.java
@@ -1,56 +1,56 @@
 package com.jcalc.pkg;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Pair;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		int total = 0;
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			AnchorPane root = new AnchorPane();
+			Scene scene = new Scene(root, 300, 300);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			Text calculator = new Text("Write a calculator here");
+			Text calculator = new Text("JCalc - JavaFX Calculator");
 			calculator.setX(50);
 			calculator.setY(50);
 			root.getChildren().add(calculator);
-			primaryStage.setTitle("JCalc - Java FX calculator");
+			primaryStage.setTitle("JCalc - JavaFX calculator");
 			primaryStage.setScene(scene);
-			Button equalsButton = new Button();			
-			equalsButton.setLayoutY(200);
-			equalsButton.setLayoutY(200);
-			root.getChildren().add(equalsButton);
+			Button btn = new Button();
+			btn.setText("=");
+			btn.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					System.out.println("The answer is: " + 1); // add as total
+				}
+			});
+			btn.setLayoutX(250);
+			btn.setLayoutY(220);	
+			btn.setText("1");
+			btn.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					// total += 1; not able to use due to scope issues
+				}
+			});
+			btn.setLayoutX(250);
+			btn.setLayoutY(220);
+			root.getChildren().add(btn);
 			
-			  Button btn = new Button();
-			    btn.setText("'Hello World'");
-			    btn.setOnAction(new EventHandler<ActionEvent>() {
+			primaryStage.show();
 
-			        @Override
-			        public void handle(ActionEvent event) {
-			            System.out.println("Hello World!");
-			        }
-			    });
-			
-			    btn.setLayoutX(250);
-			    btn.setLayoutY(220);
-			    root.getChildren().add(btn);
-			  //  primaryStage.setScene(new Scene(root, 300, 250));
-			    primaryStage.show();
-			
-			
-			
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
