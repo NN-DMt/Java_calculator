2820c9126ac58c7276aa8c450e6189dc542d8e1b
NN-DMt <oj17@hotmail.co.uk>
2017-09-17 20:29:50 +0100

    updated prototype reference code

--- src/test/java/com/jcalc/pkg/prototypingcode/Prototype_code.java
+++ src/test/java/com/jcalc/pkg/prototypingcode/Prototype_code.java
@@ -163,6 +163,87 @@ public class Prototype_code {
 	
 	/* V3 */
 	
+/*	package com.jcalc.pkg;
+
+	public class EquationStringParser {
+		private String eqString;
+		private String[] eqTokens;
+		
+		public EquationStringParser(String equationString) {
+			this.eqString = equationString.trim();
+			this.eqTokens = eqString.split(" ");
+		}
+		
+		public EquationStringParser(String[] equationTokens) {
+			this.eqTokens = equationTokens;
+		}
+
+		public double parseEqToDouble() {
+				double firstNum = 0.0d;
+				double secondNum = 0.0d;
+				double finalTotal = 0.0d;
+				double currentTotal = 0.0d;
+				boolean first = true;
+				String operator = null;
+				for(String s : eqTokens) {
+					if (firstNum == 0.0d) {
+						firstNum = Double.parseDouble(s);
+					} else if (operator == null) {
+						operator = s;
+					} else if (secondNum == 0.0d) {
+						secondNum = Double.parseDouble(s);
+					}
+					if(firstNum != 0.0d && secondNum != 0.0d && operator != null){
+						if(first) {
+							currentTotal = returnCurrentCalculation(firstNum, operator, secondNum);
+						} else {
+							currentTotal = returnCurrentCalc(firstNum, operator, secondNum);
+						}
+						first = false;
+						firstNum = secondNum;
+						secondNum = 0.0d;
+						operator = null;
+						finalTotal += currentTotal;
+					}
+				}
+			return finalTotal;
+		}
+
+		private static double returnCurrentCalculation(Double firstNum, String operator, Double secondNum) {
+			double currentAnswer = 0.0d;
+			if(operator.equalsIgnoreCase("*")) {
+				currentAnswer = firstNum * secondNum; 
+			} else if (operator.equalsIgnoreCase("-")) {
+				currentAnswer = firstNum - secondNum;
+			} else if (operator.equalsIgnoreCase("+")) {
+				currentAnswer = firstNum + secondNum;
+			} else if (operator.equalsIgnoreCase("/")) {
+				currentAnswer = firstNum / secondNum;
+			} else {
+				System.out.println("operator not recognised, please try again");
+			}
+			return currentAnswer;
+		}
+		
+		private static double returnCurrentCalc(Double firstNum, String operator, Double secondNum) {
+			double currentAnswer = 0.0d;
+			if(operator.equalsIgnoreCase("*")) {
+				currentAnswer *= secondNum; 
+			} else if (operator.equalsIgnoreCase("-")) {
+				currentAnswer -= secondNum;
+			} else if (operator.equalsIgnoreCase("+")) {
+				currentAnswer += secondNum;
+			} else if (operator.equalsIgnoreCase("/")) {
+				currentAnswer /= secondNum;
+			} else {
+				System.out.println("operator not recognised, please try again");
+			}
+			return currentAnswer;
+		}
+	}*/
+
+	
+	
 	
 	
 }
